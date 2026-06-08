7d925afbd134e758fe46e97b408fd17419941b8c
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-06-08 18:15:40 +0900

    Add files via upload

---
+++ SS16-EX3/SS16-EX3.js
@@ -0,0 +1,7 @@
+// Viết một hàm sum() nhận vào hai số và trả về tổng của chúng, 
+// và thực hiện hiển thị lên màn hình bằng hàm alert()
+
+function sumofTwoNumber (firstNumber,secondNumber) {
+    return firstNumber + secondNumber;
+}
+console.log (sumofTwoNumber(6,8));
